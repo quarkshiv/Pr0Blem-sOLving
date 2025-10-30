bfd5e49d2dedeed19dc41132927e1c38356ce95a
quarkshiv <quark.shiv@gmail.com>
2025-10-30 19:21:53 +0530

    Time: 52 ms (18.63%), Space: 85.9 MB (70.88%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        if(nums.size()==1){
+            return 0;
+        }
+        int maxdist=0;
+        for(int i=1;i<nums.size();i++){
+            int dist = nums[i]-nums[i-1];
+            maxdist = max(maxdist,dist);
+                    }
+                    return maxdist;
+    }
+};
