4f866e693151f28fdd445f78becbbe856480721b
quarkshiv <quark.shiv@gmail.com>
2025-11-16 23:52:44 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12%) - LeetHub

--- 1033-broken-calculator/1033-broken-calculator.cpp
+++ 1033-broken-calculator/1033-broken-calculator.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int brokenCalc(int startValue, int target) {
-        int minop=0;
+        int cnt=0;
         while(target>startValue){
             if(target%2){
                 target++;
-                minop++;
             }
             else{
                 target/=2;
-                minop++;
             }
-            
+            cnt++;
         }
-        return minop + startValue-target;
+        return cnt + startValue-target;
     }
 };
