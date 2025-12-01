0102b517f443498b9d481a97f3b2aa83283441b0
quarkshiv <quark.shiv@gmail.com>
2025-12-01 11:16:57 +0530

    Time: 8 ms (85.77%), Space: 45.6 MB (25.85%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        vector<pair<int,int>>v;
+        for(int i=0;i<nums.size();i++){
+            v.push_back({nums[i],cost[i]});
+        }
+        sort(v.begin(),v.end());
+         long long totalCost = 0;
+        for(int i = 0; i < v.size(); i++) totalCost += v[i].second;
+        long long pref = 0;
+        int med = 0;
+        for(int i = 0; i <v.size(); i++){
+            pref += v[i].second;
+            if(pref * 2 >= totalCost){
+                med = v[i].first;
+                break;
+            }
+        }
+        long long tot=0;
+         for(int i=0;i<v.size();i++){
+               long long op  =  abs(med- v[i].first);
+                long long costop =  op * 1LL * v[i].second;
+                tot+=costop;
+                        }
+                        return tot;
+    }
+};
