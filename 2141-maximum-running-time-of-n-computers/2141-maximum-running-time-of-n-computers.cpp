a91065948f0992999b113ce4aa6f07b6798dd4b8
quarkshiv <quark.shiv@gmail.com>
2025-12-11 12:44:29 +0530

    Time: 27 ms (40.75%), Space: 59.7 MB (53.56%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
-    long long maxRunTime(int n, vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        long long total = accumulate(arr.begin(), arr.end(), 0LL);
-
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (arr[i] <= total / n) break;
-            total -= arr[i];
-            n--;
+    long long maxRunTime(int n, vector<int>& batteries) {
+        sort(batteries.begin(),batteries.end());
+        long long tot = 0;
+        for(auto &it : batteries){
+            tot+=it;
         }
-
-        return total / n;
+        for(int i=batteries.size()-1;i>=0;i--){
+              if(batteries[i]<=(tot/n)) break;
+              tot-=batteries[i];
+              n--;
+        }
+        return tot/n;
     }
 };
