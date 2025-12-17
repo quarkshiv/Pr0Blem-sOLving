e79f952499ed88d178591bad924e00994bb7e33c
quarkshiv <quark.shiv@gmail.com>
2025-12-17 18:16:16 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.76%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -4,8 +4,8 @@ public:
         int pref=1;
         int suff=1;
         int ans=INT_MIN;
-        int n= nums.size();
-        for(int i=0;i<n;i++){
+        int n = nums.size();
+        for(int i=0;i<nums.size();i++){
             if(pref==0) pref=1;
             if(suff==0) suff=1;
             pref*=nums[i];
