eb9af74a2cb9076b402936dba1c1fbedca1eb90b
quarkshiv <quark.shiv@gmail.com>
2025-12-20 10:54:19 +0530

    Time: 3 ms (63.02%), Space: 15.6 MB (45.93%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int ans=0;
+        for(int i=0;i<strs[0].size();i++){
+              bool valid =true;
+              for(int j=1;j<strs.size();j++){
+                  if(strs[j][i]<strs[j-1][i]){
+                    valid = false;
+                  }
+              }
+              if(!valid){
+                ans++;
+              }
+        }
+        return ans;
+    }
+};
