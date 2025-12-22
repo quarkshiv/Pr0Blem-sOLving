17da2c27717dc052df87e70ca44d95f11fa03c0e
quarkshiv <quark.shiv@gmail.com>
2025-12-23 00:47:59 +0530

    Time: 233 ms (1.88%), Space: 30.5 MB (17.1%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+     int n = nums.size();
+        vector<int> temp(n);
+
+        for(int i = 0; i < n; i++) {
+            temp[(i + k) % n] = nums[i];
+        }
+        nums = temp;
+
+        for(int i = 0; i < n; i++) {
+            cout << nums[i] << " ";
+        }
+        cout << endl;
+    
+    }
+};
