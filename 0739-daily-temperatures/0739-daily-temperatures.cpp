510ab96bf510744383e9917f29495bd37976fd96
quarkshiv <quark.shiv@gmail.com>
2025-12-22 23:03:08 +0530

    Time: 23 ms (55.1%), Space: 102.7 MB (92.62%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        stack<int>st;
+        int n = t.size();
+         vector<int>ans(n,0);
+         for(int i=n-1;i>=0;i--){
+            while(!st.empty() &&  t[st.top()]<=t[i]){
+                  st.pop();
+            }
+            if(!st.empty())
+            ans[i] = st.top()-i;
+            st.push(i);
+         }
+         return ans;
+    }
+};
