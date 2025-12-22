b34e8b8e048b1f4e34b55eef13e347f888c57776
quarkshiv <quark.shiv@gmail.com>
2025-12-22 22:24:09 +0530

    Time: 4 ms (16.28%), Space: 9.9 MB (25.89%) - LeetHub

--- 1006-clumsy-factorial/1006-clumsy-factorial.cpp
+++ 1006-clumsy-factorial/1006-clumsy-factorial.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
     int clumsy(int n) {
-        stack<int> st;
+        stack<int>st;
         st.push(n);
         n--;
-        int op=0;
-        while (n > 0) {
-            if (op==0) {
+        int parity=0;
+        while(n>0){
+             if(parity==0){
                 int t = st.top();
-                 st.pop();
-                st.push(t * n);
-            }
-            else if (op==1) {
-                int t = st.top();
-                 st.pop();
-                st.push(t / n);
-            }
-            else if (op==2) {
-                st.push(n);
-            }
-            else {
+                st.pop();
+                st.push(t*n);
+             }
+             else if(parity==1){
+                int t= st.top();
+                st.pop();
+                st.push(t/n);
+             }
+             else if(parity==2){
+               st.push(n);
+             }
+             else{
                 st.push(-n);
-            }
-
-            op = (op + 1) % 4;
-            n--;
+             }
+             parity = (parity +1)%4;
+             n--;
         }
-        int result = 0;
-        while (!st.empty()) {
-            result += st.top();
-            st.pop();
+        int res=0;
+        while(!st.empty()){
+               res+=st.top();
+               st.pop();
         }
-        return result;
+        return res;
     }
 };
