c9dd59c1be62922590427b7ce3685cb54841c602
quarkshiv <quark.shiv@gmail.com>
2025-12-23 18:47:33 +0530

    Time: 192 ms (48.04%), Space: 169.5 MB (22.5%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-   int f(vector<vector<int>>& events,int ind,int k,vector<vector<int>> & dp){
+   int f(vector<vector<int>>& events,int ind,int k,vector<vector<int>> & dp,vector<int>&starts){
     int n = events.size();
        if(ind>=n  || k==0){
         return 0;
@@ -8,12 +8,9 @@ public:
        if(dp[ind][k]!=-1){
         return dp[ind][k];
        }
-       int skip = f(events,ind +1,k,dp);
-        int j;
-        for(j=ind +1;j<n;j++){
-            if(events[j][0]>events[ind][1]) break; //non overlapping interval
-        }
-        int take = events[ind][2] + f(events,j,k-1,dp);
+       int skip = f(events,ind +1,k,dp,starts);
+        int j= upper_bound(starts.begin(),starts.end(),events[ind][1])-starts.begin();
+        int take = events[ind][2] + f(events,j,k-1,dp,starts);
         return dp[ind][k]=max(skip,take);
 
    }
@@ -21,6 +18,10 @@ public:
         int n = events.size();
         vector<vector<int>>dp(n+1,vector<int>(k+1,-1));
         sort(events.begin(),events.end());
-        return f(events,0,k,dp);
+        vector<int>starts(n);
+        for(int i=0;i<n;i++){
+            starts[i]=events[i][0];
+        }
+        return f(events,0,k,dp,starts);
     }
 };
