ac77fd444103b1165dda22c8f23b3be6cad2509d
quarkshiv <quark.shiv@gmail.com>
2025-12-24 18:04:25 +0530

    Time: 0 ms (100%), Space: 10.4 MB (88.97%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,30 +1,29 @@
+typedef long long ll;
 class Solution {
 public:
-   int countpar(vector<int>&nums,int mid){
-        int partition=1;
-        int subarraysum=0;
-        for(int i=0;i<nums.size();i++){
-              if(subarraysum + nums[i]<= mid){
-                   subarraysum+=nums[i];
-              }
-              else{
+   int countpar(long long mid,vector<int>& nums){
+       int partition=1;
+       int subarraysum=0;
+       for(int i=0;i<nums.size();i++){
+             if(nums[i]+subarraysum<=mid){
+                subarraysum+=nums[i];
+             }
+             else{
                 partition++;
                 subarraysum=nums[i];
-              }
-        }
-        return partition;
+             }
+       }
+       return partition;
    }
     int splitArray(vector<int>& nums, int k) {
-        int maxi = *max_element(nums.begin(),nums.end());
-        int sum  = accumulate(nums.begin(),nums.end(),0);
-        int low = maxi;
-        int high = sum;
+        ll low=*max_element(nums.begin(),nums.end());
+        ll high=accumulate(nums.begin(),nums.end(),0LL);
         while(low<=high){
-            int mid =  (low+high)/2;
-              if(countpar(nums,mid)<=k){
-                  high = mid-1;
-              }
-              else low = mid+1;
+            ll mid = low +(high-low)/2;
+            if(countpar(mid,nums)<=k){
+                  high =mid-1;
+            }
+            else low = mid+1;
         }
         return low;
     }
