ff166462bd4facefe250a0eeef9791d9eff70a06
quarkshiv <quark.shiv@gmail.com>
2025-12-25 10:22:56 +0530

    Time: 217 ms (14.89%), Space: 110.1 MB (25.18%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.rbegin(),happiness.rend());
+        long long ans=0;
+        for(int i=0;i<k;i++){
+          long long val = (happiness[i]-i);
+            if(val<=0){
+                break;
+            }
+            else ans+=val;
+        }
+        return ans;
+    }
+
+        
+};
