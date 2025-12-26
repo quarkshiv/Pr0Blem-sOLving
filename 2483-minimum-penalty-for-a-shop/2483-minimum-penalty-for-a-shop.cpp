8c9f0f0cc4a01d6e5f1d8ee3733e49bdc307465f
quarkshiv <quark.shiv@gmail.com>
2025-12-26 09:25:19 +0530

    Time: 11 ms (37.38%), Space: 21.8 MB (12.24%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int bestClosingTime(string s) {
-         int n =s.size();
-         vector<int>pref(n+1,0);
-         vector<int>suff(n+1,0);
-         int ans= INT_MAX;
-         int time=0;
-         for(int i=1;i<=n;i++){
-            pref[i]+=(pref[i-1] + (s[i-1]=='N')); 
-         }
-         for(int i=n-1;i>=0;i--){
-            suff[i]+=(suff[i+1] + (s[i]=='Y')); 
-         }
+        int n= s.size();
+        vector<int>pref(n+1,0),suff(n+1,0);
+        for(int i=1;i<=n;i++){
+            pref[i]=pref[i-1] + (s[i-1]=='N');
+        }
+        for(int i=n-1;i>=0;i--){
+            suff[i]=suff[i+1] + (s[i]=='Y');
+        }
+        int mini=INT_MAX;
+        int time=0;
         for(int i=0;i<=n;i++){
-            int pre= pref[i];
-            int suf = suff[i];
-           if(ans>pre+suf){
-            ans= pre+suf;
-            time = i;
-           }
+            int val = pref[i]+suff[i];
+            if(mini>val){mini=val;
+            time=i;}
         }
         return time;
-    }
+            }
 };
