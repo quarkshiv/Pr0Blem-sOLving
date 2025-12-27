7e4a89d887b9f486cb121ec70d1a28e15cdb8911
quarkshiv <quark.shiv@gmail.com>
2025-12-27 14:53:31 +0530

    Time: 64 ms (39.68%), Space: 75.1 MB (33.31%) - LeetHub

---
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxEvents(vector<vector<int>>& events) {
+        sort(events.begin(),events.end());
+        priority_queue<int,vector<int>,greater<int>>pq;
+        int cnt=0;
+        int i=0;
+        int n = events.size();
+        int day=1;
+        while(!pq.empty() || i<n){
+            while(i<n && events[i][0]==day){
+                pq.push(events[i][1]);
+                i++;
+            }
+            while(!pq.empty() && pq.top()<day){
+                  pq.pop();
+            }
+            if(!pq.empty()){
+                pq.pop();
+                cnt++;
+            }
+            day++;
+        }
+        return cnt;
+
+    }
+};
