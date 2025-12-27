d5954599621d0488c568393c492c193078878964
quarkshiv <quark.shiv@gmail.com>
2025-12-27 19:30:03 +0530

    Time: 185 ms (62.39%), Space: 59.8 MB (72.83%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     int numBusesToDestination(vector<vector<int>>& routes, int source, int target) {
-        int n = routes.size();
         if(source==target){
             return 0;
         }
+        int n = routes.size();
         unordered_map<int,vector<int>>adj;
-          for(int i=0;i<routes.size();i++){
-               for(int it: routes[i]){  //it is stop and i is the route
-                adj[it].push_back(i);
+        for(int i=0;i<routes.size();i++){
+            for(auto it : routes[i]){
+              adj[it].push_back(i);
+            }
+        }
+        vector<int>vis(n,0);
+        queue<int>q;
+        for(auto it : adj[source]){
+               vis[it]=1;
+               q.push(it);
+        }
+        int buses=1;
+        while(!q.empty()){
+               int size = q.size();
+               while(size--){
+                   int route = q.front();
+                   q.pop();
+                for(auto stop : routes[route]){
+                        if(stop==target){
+                            return buses;
+                        }
+                        for(auto nextroutes : adj[stop]){
+                                if(!vis[nextroutes]){
+                                     vis[nextroutes]=1;
+                                     q.push(nextroutes);
+                                }
+                        }
+                }
                }
-          }
-         queue<int>q;
-         int buses=1;
-         vector<int>vis(n,0);
-         for(auto &it : adj[source]){
-            q.push(it);
-            vis[it]=1;
-         } 
-         while(!q.empty()){
-              int size = q.size();
-              while(size--){
-                  int route= q.front();
-                  q.pop();
-                  for(auto stop: routes[route]){
-                       if(stop==target){
-                        return buses;
-                       }
-                       for(auto nextstop : adj[stop]){
-                              if(!vis[nextstop]){
-                                vis[nextstop]=1;
-                                q.push(nextstop);
-                              }
-                       }                         
-                  }
-
-              }
-              buses++;
-
-         }
+               buses++;
+        }
          return -1;
     }
 };
