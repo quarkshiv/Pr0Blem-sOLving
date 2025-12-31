e5a80d0845092a9cf460c916b55e3dca35414d58
quarkshiv <quark.shiv@gmail.com>
2025-12-31 22:18:35 +0530

    Time: 40 ms (20.13%), Space: 58.8 MB (19.42%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    const int mod=1e9+7;
+const int mod= 1e9+7;
     int numberOfWays(string corridor) {
-        vector<int>pos;
-        for(int i=0;i<corridor.size();i++){
-            if(corridor[i]=='S'){
-                pos.push_back(i);
-            }
-        }
-            if(pos.size()==0 || pos.size()%2!=0) return 0;
-            if(pos.size()==2)return 1;
-            long long ans=1;
-            for(int i=1;i<pos.size()-1;i+=2){
-                int left=pos[i];
-                int right =pos[i+1];
-                int cnt = (right-left-1)+1;
-                ans= (ans * 1LL*cnt)%mod;
-            }
-        return (int)ans;
+           vector<int>s;
+           for(int i=0;i<corridor.size();i++){
+            if(corridor[i]=='S')
+              s.push_back(i+1);
+           }
+            if(s.size()==0 || s.size()%2!=0) return 0;
+            if(s.size()==2)return 1;
+           long long ans=1;
+           for(int i=1;i<s.size()-1;i+=2){
+                int left=s[i];
+                int right=s[i+1];
+                int positions =(right-left-1)+1;
+                ans = (ans * 1LL*positions)%mod;
+           }
+           return ans;
     }
 };
