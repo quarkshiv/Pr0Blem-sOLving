4e156d151d4ddf307862b1d9a5c14c73014a3f29
quarkshiv <quark.shiv@gmail.com>
2025-12-31 21:20:53 +0530

    Time: 538 ms (28.28%), Space: 235.7 MB (36.33%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        vector<vector<pair<int,int>>>v(n);
-        for(auto &it : meetings){
-               v[it[0]].push_back({it[1],it[2]});
-               v[it[1]].push_back({it[0],it[2]});
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it : meetings){
+               adj[it[1]].push_back({it[0],it[2]});
+               adj[it[0]].push_back({it[1],it[2]});
         }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+        q.push({0,0});
+        q.push({0,firstPerson});
         vector<int>dist(n,INT_MAX);
-        dist[0]=0;
-        dist[firstPerson]=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
-        pq.push({0,firstPerson});
-        while(!pq.empty()){
-            int time = pq.top().first;
-            int node = pq.top().second;
-            pq.pop();
-
-            if(time >dist[node]){
-                continue;
-            }
-            for(auto &it : v[node]){
-                   if(it.second>=time && dist[it.first]>time){
-                        pq.push({it.second,it.first});
-                        dist[it.first]=it.second;
+          dist[0]=0;
+          dist[firstPerson]=0;
+          while(!q.empty()){
+                  int time = q.top().first;
+                  int person = q.top().second;
+                  q.pop();
+                  if(time>dist[person]){
+                    continue;
+                  }
+                   for(auto it : adj[person]){
+                      if(it.second>=time && dist[it.first]>time){
+                           dist[it.first] = it.second;
+                           q.push({it.second,it.first});
+                      }
                    }
-            }
-        }
-            vector<int>ans;
-            for(int i=0;i<n;i++){
-                if(dist[i]!=INT_MAX) ans.push_back(i);
-            }
-        return ans;
+          }
+          vector<int>ans;
+          for(int i=0;i<n;i++){
+             if(dist[i]!=INT_MAX){
+                 ans.push_back(i);
+             }
+          }
+          return ans;
     }
 };
