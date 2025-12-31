4c2e69b0c83d6977835eefd2a8ff3a125410d309
quarkshiv <quark.shiv@gmail.com>
2025-12-31 14:20:57 +0530

    Time: 13 ms (38.23%), Space: 19.1 MB (71.95%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int countCollisions(string directions) {
         int l=0;
-        int n = directions.size();
-        int r =n-1;
-        while(l<n && directions[l]=='L') l++;
+        int r = directions.size()-1;
+        while(l<directions.size() && directions[l]=='L') l++;
         while(r>=0 && directions[r]=='R') r--;
         int ans=0;
         while(l<=r){
