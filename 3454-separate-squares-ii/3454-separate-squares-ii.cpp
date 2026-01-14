b7681d7ca900671d289f3ad8985242e4ea3153d7
quarkshiv <quark.shiv@gmail.com>
2026-01-15 00:29:36 +0530

    Time: 210 ms (98.56%), Space: 135.5 MB (69.07%) - LeetHub

--- 3454-separate-squares-ii/3454-separate-squares-ii.cpp
+++ 3454-separate-squares-ii/3454-separate-squares-ii.cpp
@@ -1,101 +1,120 @@
 class Solution {
 public:
-    typedef pair<int, int> ii;
-    class SegmentTree{
-        public:
-            vector<ii> tree; // count - length
-
-            SegmentTree(int n) {
-                tree.resize(4 * n);
-            }
-           
-            void update(int i, int left, int right, int uLeft, int uRight, int val, vector<int>& originX) {
-                if (right < uLeft || left > uRight) {
-                    return;
-                }
-
-                if (uLeft <= left && right <= uRight) {
-                    tree[i].first += val;
-                } else {
-                    int mid = (left + right) / 2;
-                    update(2 * i + 1, left, mid, uLeft, uRight, val, originX);
-                    update(2 * i + 2, mid + 1, right, uLeft, uRight, val, originX);
-                }
-                
-                if (tree[i].first > 0) {
-                    tree[i].second = originX[right + 1] - originX[left];
-                } else if (left == right) {
-                    tree[i].second = 0;
-                } else {
-                    tree[i].second = tree[2 * i + 1].second + tree[2 * i + 2].second;
-                }
-            }
-
-            int query() {
-                return tree[0].second;
-            }
+    struct Node {
+        int count;
+        double covered;
+        Node() {
+            count = 0;
+            covered = 0;
+        }
     };
+    struct Event {
+        double y;
+        int x1, x2;
+        int type;
+    };
+    struct Slice {
+        double y1, y2, width;
+    };
+    vector<double> xs;
+    vector<Node> seg;
 
+    static bool eventCompare(const Event& a, const Event& b) {
+        return a.y < b.y;
+    }
+    void update(int idx, int l, int r, int ql, int qr, int val) {
+        if (qr <= l || r <= ql) {
+            return;
+        }
+        if (ql <= l && r <= qr) {
+            seg[idx].count += val;
+        } else {
+            int mid = (l + r) / 2;
+            update(idx * 2, l, mid, ql, qr, val);
+            update(idx * 2 + 1, mid, r, ql, qr, val);
+        }
+        if (seg[idx].count > 0) {
+            seg[idx].covered = xs[r] - xs[l];
+        } else if (l + 1 == r) {
+            seg[idx].covered = 0;
+        } else {
+            seg[idx].covered = seg[idx * 2].covered + seg[idx * 2 + 1].covered;
+        }
+    }
+    int getXIndex(double x) {
+        return lower_bound(xs.begin(), xs.end(), x) - xs.begin();
+    }
     double separateSquares(vector<vector<int>>& squares) {
-        int n = squares.size();
-        
-        vector<tuple<int, int, int, int>> events;
-        vector<int> originX;
-        int maxX = 0;
-        for (int i = 0; i < n; ++i) {
-            int x = squares[i][0];
-            int y = squares[i][1];
-            int l = squares[i][2];
-
-            events.emplace_back(y, x, l, 1);
-            events.emplace_back(y + l, x, l, -1);
-
-            originX.push_back(x);
-            originX.push_back(x + l);
-
-            maxX = max(maxX, x + l);
+        vector<Event> events;
+        xs.clear();
+        for (int i = 0; i < squares.size(); i++) {
+            double x = squares[i][0];
+            double l = squares[i][2];
+            xs.push_back(x);
+            xs.push_back(x + l);
         }
-        
-        sort(events.begin(), events.end(), [](const auto& a, const auto& b) {
-            return get<0>(a) < get<0>(b);
-        });
-        sort(originX.begin(), originX.end());
+        sort(xs.begin(), xs.end());
+        xs.erase(unique(xs.begin(), xs.end()), xs.end());
 
-        int maxCompressX = lower_bound(originX.begin(), originX.end(), maxX) - originX.begin();
-        SegmentTree* segmentTree = new SegmentTree(maxCompressX);
+        for (int i = 0; i < squares.size(); i++) {
+            double x = squares[i][0];
+            double y = squares[i][1];
+            double l = squares[i][2];
 
-        // Calculate totalArea
-        double totalArea = 0;
-        double prevY = 0;
-        for (auto [y, x, l, type] : events) {
-            double thisArea = segmentTree->query() * (y - prevY);
-            
-            int left = lower_bound(originX.begin(), originX.end(), x) - originX.begin();
-            int right = lower_bound(originX.begin(), originX.end(), x + l) - originX.begin() - 1;
-            segmentTree->update(0, 0, maxCompressX, left, right, type, originX);
-            totalArea += thisArea;
-            prevY = y;
+            int x1 = getXIndex(x);
+            int x2 = getXIndex(x + l);
+
+            Event e1;
+            e1.y = y;
+            e1.x1 = x1;
+            e1.x2 = x2;
+            e1.type = 1;
+
+            Event e2;
+            e2.y = y + l;
+            e2.x1 = x1;
+            e2.x2 = x2;
+            e2.type = -1;
+
+            events.push_back(e1);
+            events.push_back(e2);
         }
 
-        // Find answer
-        segmentTree = new SegmentTree(maxCompressX);
-        double curArea = 0;
-        double curWidth = 0;
-        prevY = 0;
-        for (auto [y, x, l, type] : events) {
-            curWidth = segmentTree->query();
-            double thisArea =  curWidth * (y - prevY);
-            if (2 * (curArea + thisArea) >= totalArea) {
-                return prevY + (totalArea - 2.0 * curArea) / (2.0 * curWidth);
+        sort(events.begin(), events.end(), eventCompare);
+
+        seg.assign(xs.size() * 4, Node());
+        vector<Slice> slices;
+        double prevY = events[0].y;
+
+        for (int i = 0; i < events.size(); i++) {
+            double curY = events[i].y;
+            double width = seg[1].covered;
+            if (curY > prevY && width > 0) {
+                Slice s;
+                s.y1 = prevY;
+                s.y2 = curY;
+                s.width = width;
+                slices.push_back(s);
             }
-
-            int left = lower_bound(originX.begin(), originX.end(), x) - originX.begin();
-            int right = lower_bound(originX.begin(), originX.end(), x + l) - originX.begin() - 1;
-            segmentTree->update(0, 0, maxCompressX, left, right, type, originX);
-            curArea += thisArea;
-            prevY = y;
+            update(1, 0, xs.size() - 1, events[i].x1, events[i].x2,
+                   events[i].type);
+            prevY = curY;
         }
+        double totalArea = 0;
+        for (int i = 0; i < slices.size(); i++) {
+            totalArea += slices[i].width * (slices[i].y2 - slices[i].y1);
+        }
+        double halfArea = totalArea / 2.0;
+        double currArea = 0;
 
-        return 0;
+        for (int i = 0; i < slices.size(); i++) {
+            double area = slices[i].width * (slices[i].y2 - slices[i].y1);
+            if (currArea + area >= halfArea) {
+                double remArea = halfArea - currArea;
+                return slices[i].y1 + remArea / slices[i].width;
+            }
+            currArea += area;
+        }
+        return 0.0;
     }
 };
