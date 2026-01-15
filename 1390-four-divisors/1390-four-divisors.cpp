e3899b000f489c970e8669e90adf321861e4d844
quarkshiv <quark.shiv@gmail.com>
2026-01-15 23:45:39 +0530

    Time: 217 ms (12.92%), Space: 89.7 MB (5.01%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,21 +1,22 @@
 class Solution {
-public: 
-   void cntdiv(int val,int &sum){
-            vector<int>v;
-          for(int i=1;i*i<=val;i++){
-               if(val%i==0){
-                v.push_back(i);
-                if(i!=val/i) v.push_back(val/i);
-               }
-          }
-          if(v.size()==4){
-               sum+=accumulate(v.begin(),v.end(),0);
-          }
-   }
+public:
+    int sum=0;
+    void cntdiv(int n){
+        set<int>v;
+           for(int i=1;i*i<=n;i++){
+                 if(n%i==0){
+                      v.insert(i);
+                      v.insert(n/i);
+                 }
+           }
+           if(v.size()==4){
+               vector<int>arr(v.begin(),v.end());
+               sum+=accumulate(arr.begin(),arr.end(),0);
+           }
+    }
     int sumFourDivisors(vector<int>& nums) {
-        int sum=0;
         for(int i=0;i<nums.size();i++){
-             cntdiv(nums[i],sum);
+            cntdiv(nums[i]);
         }
         return sum;
     }
