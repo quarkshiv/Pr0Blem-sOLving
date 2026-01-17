925ecd45ae04c6688916f9b31fbb54155a57fd02
quarkshiv <quark.shiv@gmail.com>
2026-01-17 22:21:11 +0530

    Time: 151 ms (17.41%), Space: 259.8 MB (38.21%) - LeetHub

---
+++ 3810-minimum-operations-to-reach-target-array/3810-minimum-operations-to-reach-target-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& target) {
+            set<int>st;
+            for(int i=0;i<nums.size();i++){
+                     if(nums[i]!=target[i])
+                     st.insert(nums[i]);
+            }
+                     return st.size();
+
+    }
+};
