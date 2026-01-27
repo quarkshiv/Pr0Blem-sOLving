313c1581119244227a312ddeed98fc4d20a1039a
quarkshiv <quark.shiv@gmail.com>
2026-01-27 16:42:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+           int n  = heights.size();
+           int m= heights[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        pq.push({0,{0,0}});
+        while(!pq.empty()){
+              auto val =pq.top();
+                  int diff= val.first;
+                  int r=val.second.first;
+                  int c=val.second.second;
+                  pq.pop();
+                  if(r==n-1 && c==m-1){
+                    return diff;
+                  }
+                for(int delrow=-1;delrow<=1;delrow++){
+                for(int delcol=-1;delcol<=1;delcol++){
+                     int nrow = r + delrow;
+                    int ncol = c+delcol;
+                    if(nrow<n && nrow>=0 && ncol<m && ncol>=0){
+                              int neweffort = max(abs(heights[r][c] - heights[nrow][ncol]),diff);
+                              if(neweffort<dist[nrow][ncol]){
+                                dist[nrow][ncol] = neweffort;
+                                pq.push({neweffort,{nrow,ncol}});
+
+        }
+                    }
+        }
+                }}
+        return -1;
+    }
+};
