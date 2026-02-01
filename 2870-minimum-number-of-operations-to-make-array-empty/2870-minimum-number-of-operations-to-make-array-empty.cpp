563c0f4414002dc2ad8541f17d9540110368592b
quarkshiv <quark.shiv@gmail.com>
2026-02-01 11:30:20 +0530

    Time: 20 ms (42.8%), Space: 95.8 MB (59.11%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        for(auto it : nums){
+             mpp[it]++;
+        }
+        int op=0;
+        for(auto it : mpp){
+            if(it.second==1)
+        return -1;
+              if(it.second%3==0){
+                    op+=it.second/3;
+              }
+              else op+=((it.second/3)+1);
+        }
+        return op;
+    }
+};
