04e42ae3d7d2c8e58bbf5a0b6deed23357ca266a
quarkshiv <quark.shiv@gmail.com>
2026-02-11 19:47:46 +0530

    Time: 5 ms (72.97%), Space: 14.2 MB (21.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-  int f(int ind1,int ind2,string&w1,string&w2, vector<vector<int>>&dp){
-         int n= w1.size();
-        int m = w2.size();
-          if(ind1==n){
-            return m-ind2;
-          }
-          if(ind2==m){
-            return n-ind1;
-          }
-          if(dp[ind1][ind2]!=-1){
-            return dp[ind1][ind2];
-          }
-          if(w1[ind1]==w2[ind2]){
-             return dp[ind1][ind2]= f(ind1+1,ind2+1,w1,w2,dp);
-          }
-         else{
-              return dp[ind1][ind2] = 1 + min({f(ind1+1,ind2,w1,w2,dp),f(ind1,ind2+1,w1,w2,dp), f(ind1+1,ind2+1,w1,w2,dp)});
-         }
-  }
+     int f(int i,int j,string& s, string& t,vector<vector<int>>&dp){
+         int n = s.size();
+        int m =t.size();
+        if(i==n){
+            return m-j;
+        }
+        if(j==m){
+            return n-i;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==t[j]){
+             return dp[i][j]= f(i+1,j+1,s,t,dp);
+        }
+        else{
+             return dp[i][j] = min({1+f(i+1,j,s,t,dp),1+f(i,j+1,s,t,dp),1+f(i+1,j+1,s,t,dp)});
+        }
+     }
     int minDistance(string word1, string word2) {
-        int n= word1.size();
-        int m = word2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        if(word1.empty()){
+            return word2.size();
+        }
+        if(word2.empty()){
+            return word1.size();
+        }
+        int n = word1.size();
+        int m =word2.size();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
         return f(0,0,word1,word2,dp);
+
     }
 };
