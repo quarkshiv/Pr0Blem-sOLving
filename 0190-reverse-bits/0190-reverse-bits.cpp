cbc997f60ffc74766157f95f6eaa246c6c25776c
quarkshiv <quark.shiv@gmail.com>
2026-02-16 20:56:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (15.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int reverseBits(int n) {
-        string ans="";
-       for(int i=31;i>=0;i--){
-        ans += char('0' + ((n >> i)&1));
-       }
-       reverse(ans.begin(),ans.end());
-       unsigned int res=0;
-       for(char  c : ans){
-           res =res * 2 + (c-'0');
-       }
-       return res;
+        string s =  bitset<32>(n).to_string();
+        reverse(s.begin(),s.end());
+        int ans=0;
+        for(auto it: s){
+            ans=2*ans+(it-'0');
+        }
+        return ans;
     }
 };
