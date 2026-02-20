28d72fc7cc0a7b330d1102cb1b60c1a66077dfe9
quarkshiv <quark.shiv@gmail.com>
2026-02-20 19:35:09 +0530

    Time: 8 ms (75.57%), Space: 14.1 MB (31.31%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -4,21 +4,19 @@ public:
         sort(nums.begin(),nums.end());
         int closest=nums[0]+nums[1]+nums[2];
         for(int i=0;i<nums.size();i++){
-            int left=i+1;
-            int right=nums.size()-1;
-            while(left<right){
-            int sum= nums[left] + nums[i]+nums[right];
-            if(abs(sum-target)<abs(closest-target)){
-                closest=sum;
-            }
-            if(sum<target){
-                left++;
-            }
-            else if(sum>target){
-                right--;
-            }
-            else return sum;
-        }
+               int j=i+1;
+               int k=nums.size()-1;
+               while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                  if(abs(sum-target)<abs(closest-target)){
+                       closest=sum;
+                  }
+                  else if(sum>target){
+                       k--;
+                  }
+                  else if(sum<target)j++;
+                  else return sum;
+               }
         }
         return closest;
     }
