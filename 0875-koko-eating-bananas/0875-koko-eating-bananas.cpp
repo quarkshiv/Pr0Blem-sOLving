fbe7fde2d5c163cbd8a0c026297d37f83841f580
quarkshiv <quark.shiv@gmail.com>
2026-02-21 22:58:00 +0530

    Time: 8 ms (54.63%), Space: 22.9 MB (77.67%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+     long long cnt(vector<int>& piles,long long mid){
+            long long ans=0;
+            for(auto it : piles){
+                  ans+=(it+mid-1)/mid;
+            }
+            return ans;
+     }
+    int minEatingSpeed(vector<int>& piles, int h) {
+            long long low =1;
+            long long high=*max_element(piles.begin(),piles.end());
+            long long ans=high;
+            while(low<=high){
+                  long long mid = low + (high-low)/2;
+                    if(cnt(piles,mid)<=h){
+                          ans=mid;
+                          high=mid-1;
+                    }
+                    else low = mid+1;
+            }
+            return (int)ans;
+    }
+};
