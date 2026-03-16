c8ab9678812e92a9b8ec6e58b4a60af13e5b11a2
quarkshiv <quark.shiv@gmail.com>
2026-03-16 21:15:46 +0530

    Time: 0 ms (100%), Space: 9.1 MB (22.22%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+         long long cnt=0;
+        if (n >= 1000)cnt+=n-999;
+        if (n >= 1000000)cnt +=n - 999999;
+        if (n >= 1000000000)cnt += n - 999999999;
+        if (n >= 1000000000000LL) cnt += n - 999999999999LL;
+        if (n >= 1000000000000000LL) cnt+= n - 999999999999999LL;
+        return cnt;
+    }
+};
