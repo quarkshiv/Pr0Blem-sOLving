4602c250d98ea9c7dda5213bfab43395e493b7d4
quarkshiv <quark.shiv@gmail.com>
2026-03-24 11:59:21 +0530

    Time: 207 ms (85.91%), Space: 113.7 MB (80.39%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
      int merge(int low,int mid,int high,vector<int>&nums){
         int cnt=0;
-          int j=mid+1;
-          for(int i=low;i<=mid;i++){
-             while(j<=high && (long long)nums[i] >2LL * nums[j]){ j++;}
-          cnt+=(j-(mid+1));
-     }
-          int l=low;
+        int j = mid+1;
+        for(int i=low;i<=mid;i++){
+               while(j<=high && 2ll*nums[j]<(long long)nums[i])j++;
+               cnt+=j-(mid+1);
+        }
+         int l=low;
           int r = mid+1;
           vector<int>ans(high-low+1);
             int k=0;
@@ -24,19 +24,19 @@ public:
         }
         return cnt;
      }
-   int mergesort(int low,int high,vector<int>& nums){
-        if(low>=high){
-            return 0;
-        }
-        int mid = (low+high)/2;
-        int count=0;
-        count+=mergesort(low,mid,nums);
-        count+=mergesort(mid+1,high,nums);
-        count+=merge(low,mid,high,nums);
-        return count;
+   int mergesort(int l,int r,vector<int>& nums){
+       
+       if(l>=r){
+        return 0;
+       }
+       int cnt=0;
+       int mid = l + (r-l)/2;
+          cnt+=mergesort(l,mid,nums);
+          cnt+=mergesort(mid+1,r,nums);
+          cnt+=merge(l,mid,r,nums);
+          return cnt;
    }
     int reversePairs(vector<int>& nums) {
-      return mergesort(0,nums.size()-1,nums);
-
+          return mergesort(0,nums.size()-1,nums);
     }
 };
