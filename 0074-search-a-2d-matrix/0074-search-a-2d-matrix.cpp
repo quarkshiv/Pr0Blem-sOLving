b326d10be117547f1bd56f76dc2d3cdb43f3d45f
quarkshiv <quark.shiv@gmail.com>
2026-04-04 00:20:22 +0530

    Time: 0 ms (100%), Space: 13.3 MB (44.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
-    bool bs(vector<int>&arr,int target){
-             int low=0;
-             int high = arr.size()-1;
-             while(low<=high){
-                  int mid=  (low + high)/2;
-                  if(arr[mid]==target)return true;
-                  else if(arr[mid]>target){
-                      high = mid-1;
-                  }
-                  else low = mid+1;
-             }
-             return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int n = matrix.size();
-        int m  = matrix[0].size();
-        for(int i=0;i<n;i++){
-              if(bs(matrix[i],target))return true;
-        }
-        return false;
+        int m = matrix[0].size();
+            int low =0;
+            int high = n*m-1;
+            while(low<=high){
+                 int mid=  (low + high)/2;
+                 int row = mid/m;
+                 int col = mid%m;
+                 if(matrix[row][col]==target)return true;
+                 else if(matrix[row][col]>target)high=mid-1;
+                 else low = mid+1;
+            }
+            return false;
     }
 };
