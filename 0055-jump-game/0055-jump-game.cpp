ced279a420ecafba4020a47ea2386f0152967462
quarkshiv <quark.shiv@gmail.com>
2026-04-05 03:54:44 +0530

    Time: 0 ms (100%), Space: 52.3 MB (81.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int far=0;
-        for(int i=0;i<nums.size();i++){
-            if(far<i){
-                return false;
-            }
-              far= max(far,nums[i]+i);
-              
-        }
-        return true;
+        int maxindex=0;
+           for(int i=0;i<nums.size();i++){
+            if(maxindex<i)return false;
+                 maxindex = max(maxindex,i+nums[i]);
+           }
+           return true;
     }
 };
