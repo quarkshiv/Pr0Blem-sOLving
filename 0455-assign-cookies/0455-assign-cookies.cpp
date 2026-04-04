1db907fed7fb86d0d1f554267a78c05520411695
quarkshiv <quark.shiv@gmail.com>
2026-04-04 13:27:36 +0530

    Time: 7 ms (71.31%), Space: 44.8 MB (77.87%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int l=0;
+        int ans=0;
+        int r=0;
+        while(l<g.size() && r<s.size()){
+                if(s[r]>=g[l]){
+                      ans++;
+                      r++;
+                      l++;
+                }
+                else r++;
+        }
+        return ans;
+            }
+};
