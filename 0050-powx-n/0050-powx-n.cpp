cdae272a5984afaf22b8797cebb4c85ab9370feb
quarkshiv <quark.shiv@gmail.com>
2026-04-06 02:15:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,17 +3,18 @@ public:
     double myPow(double x, int n) {
         if(n==0){
             return 1;
-        }
+        }  
         if(n<0){
-            x=1/x;
-            if(n==INT_MIN)
+              x=1/x;
+               if(x==INT_MIN){
                 return x * myPow(x,INT_MAX);
-            n=-n;
+               }
+              n=-n;
         }
-          double half= myPow(x,n/2);
-          if(n%2){
-                return x*half*half;
-          }
-          else return half*half;
+        double half = myPow(x,n/2);  
+        if(n%2){
+               return half*half*x;
+        }   
+        else return half*half;       
     }
 };
