6e2e9722a9428241bbe943e18978aab964096d7b
quarkshiv <quark.shiv@gmail.com>
2026-04-08 19:02:35 +0530

    Time: 3 ms (69.68%), Space: 15.5 MB (63.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-   void f(int open,int close,int n, string s,vector<string>& ans){
-      if(s.size()==2*n){
-           ans.push_back(s);
-          return;
-      }
-      if(open<n){
-        f(open+1,close,n,s+"(",ans);
-      }
-       if(close<open){
-        f(open,close+1,n,s+")",ans);
-      }
-   }
+        void f(int open,int close,int n,string path,vector<string>& ans){
+               if(close==n){
+                   ans.push_back(path);
+                   return;
+               }
+               if(open<n){
+                    f(open+1,close,n,path+'(',ans);
+               }
+               if(close<open){
+                    f(open,close+1,n,path +')',ans);
+               }
+        }
     vector<string> generateParenthesis(int n) {
-           string s="";
-           vector<string>ans;
-           f(0,0,n,s,ans);
-           return ans;
+        vector<string>ans;
+        string path;
+        f(0,0,n,path,ans);
+        return ans;
     }
 };
