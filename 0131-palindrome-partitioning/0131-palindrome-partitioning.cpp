51b9b6fa69a6bef11cfa4dd5f4c5fba1b6208e69
quarkshiv <quark.shiv@gmail.com>
2026-04-09 18:39:28 +0530

    Time: 50 ms (48%), Space: 57.6 MB (49.82%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-     void f(int ind,string s ,vector<vector<string>>&f_ans, vector<string>&ans){
-             if(ind==s.size()){
-                   f_ans.push_back(ans);
-                   return;
-             }
-             for(int i=ind;i<s.size();i++){
-                   if(ispal(s,ind,i)){
-                       ans.push_back(s.substr(ind,i-ind+1));
-                       f(i+1,s,f_ans,ans);
-                       ans.pop_back();
-                   }
-             }
-     }
-bool ispal(string s, int l , int r){
-    while(l<=r){
-        if(s[l++]!=s[r--]) return false;
-    }
-    return true;
-   }
+void f(int ind,string s,vector<string>&ans,vector<vector<string>>&f_ans){
+           if(ind==s.size()){
+                f_ans.push_back(ans);
+                return;
+           }
+           for(int i=ind;i<s.size();i++){
+           string v= s.substr(ind,i-ind+1);
+             
+            if(ispal(v)){    
+                ans.push_back(v);  
+                f(i+1,s,ans,f_ans); 
+                  ans.pop_back();
+               }
+             
+           }
+}
+bool ispal(string s){
+       int l=0;
+       int h=s.size()-1;
+       while(l<=h){
+             if(s[l]!=s[h])return false;
+             l++;
+             h--;
+       }
+       return true;
+}
     vector<vector<string>> partition(string s) {
-        vector<vector<string>>f_ans;
-        vector<string>ans;
-        f(0,s,f_ans,ans);
-        return f_ans;
+          vector<vector<string>>f_ans;
+          vector<string>ans;
+          f(0,s,ans,f_ans);
+          return f_ans;
     }
 };
