fd494fc43f801d2905c1d69c05b11d6f245d0e0e
quarkshiv <quark.shiv@gmail.com>
2026-04-09 17:53:43 +0530

    Time: 818 ms (25.99%), Space: 11.6 MB (30.56%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-     bool f(int i,int j,vector<vector<char>>& board, string word,int k){
-         int n = board.size();
-        int m = board[0].size();
-        if(k==word.size()){
+      bool f(int i,int j,int k,vector<vector<char>>& board,string word){
+           int n = board.size();
+           int m = board[0].size();
+           if(k==word.size()){
             return true;
-        }
-        if(i<0 || j<0 || j==m || i==n || board[i][j]!=word[k] || board[i][j]=='!'){
-            return false;
-        }
-        char c= board[i][j];
-        board[i][j]='!';
-        bool op1= f(i+1,j,board,word,k+1);
-        bool op2= f(i-1,j,board,word,k+1);
-        bool op3 = f(i,j+1,board,word,k+1);
-        bool op4 = f(i,j-1,board,word,k+1);
-        board[i][j]=c;
-        return op1 || op2 || op3 || op4;
-     }
-    bool exist(vector<vector<char>>& board, string word) {
-        int n = board.size();
-        int m = board[0].size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j]==word[0]){
-                    if(f(i,j,board,word,0)) return true;
-                }
+           }
+            if(i<0 || j<0 || i>=n || j>=m|| board[i][j]!=word[k] || board[i][j]=='!'){
+                     return false;
             }
+            char c = board[i][j];
+             board[i][j]='!';
+            bool op1 = f(i+1,j,k+1,board,word);
+            bool op2 = f(i,j+1,k+1,board,word);
+            bool op3 = f(i,j-1,k+1,board,word);
+            bool op4 = f(i-1,j,k+1,board,word);
+            board[i][j]=c;
+        return op1 || op2 || op3 || op4;
+      }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++){
+              for(int j=0;j<board[0].size();j++){
+                     if(board[i][j]==word[0]){
+                           if(f(i,j,0,board,word))return true;
+                     }
+              }
         }
         return false;
     }
