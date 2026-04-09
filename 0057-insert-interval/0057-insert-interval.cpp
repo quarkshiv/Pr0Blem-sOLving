66d85d015b104d25990afabeb45db0eecef89bf5
quarkshiv <quark.shiv@gmail.com>
2026-04-10 05:17:46 +0530

    Time: 3 ms (30.3%), Space: 22.2 MB (9.35%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        int i=0;
+          vector<vector<int>>ans;
+          int n = intervals.size();
+          for(auto it : intervals){
+               if(it[1]<newInterval[0])
+                     ans.push_back(it);
+               else if(it[0]>newInterval[1]) {
+                ans.push_back(newInterval);
+                newInterval=it;
+               }
+               else{
+                     newInterval[0] = min(newInterval[0], it[0]);
+                     newInterval[1] = max(newInterval[1], it[1]);
+               }
+
+          }
+           ans.push_back(newInterval);
+          return ans;
+
+    }
+};
