aadc16a5b090af569dbb8e98cbab6dec8dee7a0d
quarkshiv <quark.shiv@gmail.com>
2026-04-16 16:55:47 +0530

    Time: 0 ms (100%), Space: 32.7 MB (62.14%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int atmost(vector<int>&nums,int goal){
-        if(goal<0)return 0;
-             int sum=0;
-             int l=0;
-             int cnt=0;
-             for(int r=0;r<nums.size();r++){
-                   sum+=nums[r];
-                   while(sum>goal){
-                      sum-=nums[l];
-                      l++;
-                   }
-                   cnt+=(r-l+1);
-             }
-             return cnt;
-  }  
+        int atmost(vector<int>& nums, int goal){
+               int l=0;
+               long long sum=0;
+               int ans=0;
+               for(int r=0;r<nums.size();r++){
+                      sum+=nums[r];
+                      while(sum>goal && l<=r){
+                            sum-=nums[l];
+                            l++;
+                      }
+                      ans+=(r-l+1);
+               }
+               return ans;
+        }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         return atmost(nums,goal)-atmost(nums,goal-1);
     }
