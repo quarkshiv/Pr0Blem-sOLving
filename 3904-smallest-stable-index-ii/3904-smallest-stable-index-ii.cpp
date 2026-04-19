e9ea21281b13980eb9e273a09fdf32c8756f0d63
quarkshiv <quark.shiv@gmail.com>
2026-04-19 22:52:09 +0530

    Time: 14 ms (37.08%), Space: 210.9 MB (39.42%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int>prefm(n),suffm(n);
+        prefm[0]=nums[0];
+        for(int i=1;i<n;i++){
+              prefm[i]= max(prefm[i-1],nums[i]);
+        }
+        suffm[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+             suffm[i]=min(suffm[i+1],nums[i]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(prefm[i]-suffm[i]<=k){
+                  return i;
+            }
+        }
+        return -1;
+    }
+};
