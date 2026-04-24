4c6a025dca7a748db44093e36c088e9aa7f3497b
quarkshiv <quark.shiv@gmail.com>
2026-04-24 12:35:49 +0530

    Time: 197 ms (10.72%), Space: 147.7 MB (6.94%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp; 
-        for (int i = 0; i < nums.size(); i++) {
-            if (mpp.find(nums[i]) != mpp.end()) {
-                if (i - mpp[nums[i]] <= k) {
-                    return true;
+          unordered_map<int,vector<int>>mpp;
+          for(int i=0;i<nums.size();i++){
+               mpp[nums[i]].push_back(i);
+          }
+          for(auto it : mpp){
+                for(int i=0;i<it.second.size();i++){
+                        for(int j=i+1;j<it.second.size();j++){
+                               if(abs(it.second[i]-it.second[j])<=k)return true;
+                        }
                 }
-            }
-            mpp[nums[i]] = i;
-        }
+          }
         return false;
     }
 };
