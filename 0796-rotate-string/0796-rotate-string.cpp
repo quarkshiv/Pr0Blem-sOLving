b3591cb2a8f7f278c31724c22d1b5d55166fc497
quarkshiv <quark.shiv@gmail.com>
2026-05-04 02:49:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.83%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+         if (s.length() != goal.length()) {
+            return false;
+        }
+        string word = s + s;
+        for (int i = 0; i < s.length(); i++) {
+            if (word.substr(i,s.length())==goal) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
