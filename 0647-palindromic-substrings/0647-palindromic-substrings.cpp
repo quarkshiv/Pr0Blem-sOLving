698c37276d65d75f48cff99f658c7381effe9560
quarkshiv <quark.shiv@gmail.com>
2026-05-19 11:05:31 +0530

    Time: 11 ms (52.05%), Space: 21.2 MB (29.88%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
-    bool ispal(string s){
-        int left=0;
-        int right = s.size()-1;
-        while(left<=right){
-              if(s[left]!=s[right]){
-                return false;
-              }
-              left++;
-              right--;
-        }
-        return true;
-    }
+int cnt=0;
     int countSubstrings(string s) {
-        int cnt=0;
         for(int i=0;i<s.size();i++){
-              string str="";
-            for(int j=i;j<s.size();j++){
-                  str+=s[j];
-                  if(ispal(str)){
-                cnt++;
-            }
-            }
-            
+              f(i,i,s);
+              f(i,i+1,s);
         }
         return cnt;
+    }
+    void f(int l,int r,string s){
+          while(l>=0 && r<s.size() && s[l]==s[r]){cnt++;
+          l--;
+          r++;
+    }
     }
 };
