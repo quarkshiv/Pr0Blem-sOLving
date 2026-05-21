be7ca6d0fff44e078be94e48bc141c0e2c931d26
quarkshiv <quark.shiv@gmail.com>
2026-05-22 02:35:42 +0530

    Time: 4 ms (50.25%), Space: 10 MB (38.82%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+          if(n==1){
+            return "1";
+          }
+          string say = countAndSay(n-1);
+          string result="";
+          for(int i=0;i<say.size();i++){
+              int cnt=1;
+              char ch=say[i];
+              while(i<say.length()-1 && say[i]==say[i+1]){
+                cnt++;
+                i++;
+              }
+              result+=to_string(cnt)+ ch;
+          }
+          return result;
+    }
+};
