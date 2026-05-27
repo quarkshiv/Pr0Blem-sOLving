fccddedd35839709f69870a0310695416e2d9125
quarkshiv <quark.shiv@gmail.com>
2026-05-27 23:10:18 +0530

    Time: 35 ms (46.82%), Space: 49.4 MB (26.71%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-       int f(int ind,vector<int>& coins,int amount,vector<vector<int>>&dp){
-           if(ind==0){
-                if(amount%coins[0]==0)return amount/coins[0];
-                else return 1e9;
-           }
-            if(dp[ind][amount] !=-1){
-        return dp[ind][amount];
-    }
-           int nottake =0+f(ind-1,coins,amount,dp);
-           int take=INT_MAX;
-              if(coins[ind]<=amount){
-           take = 1+ f(ind,coins,amount-coins[ind],dp);
-       }
-       return dp[ind][amount]=min(take,nottake);
-       }
+  int f(int ind,vector<int>& coins,int amount, vector<vector<int>>&dp){
+          if(ind==0){
+            if(amount%coins[0]==0)
+              return amount/coins[0];
+                 return 1e6;
+          }
+        if(dp[ind][amount]!=-1)return dp[ind][amount];
+        int nottake = f(ind-1,coins,amount,dp);
+        int take = INT_MAX;
+        if(coins[ind]<=amount)
+        take = 1+ f(ind,coins,amount-coins[ind],dp);
+        return dp[ind][amount]= min(take,nottake);
+  }
     int coinChange(vector<int>& coins, int amount) {
-        // hehe greedy wont work lets do dppppp
-        int n= coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans= f(n-1,coins,amount,dp);
-        return (ans==1e9) ? -1 : ans;
+        int n = coins.size();
+             vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+             int ans = f(n-1,coins,amount,dp);
+             if(ans==1e6)return -1;
+             return ans;
     }
 };
