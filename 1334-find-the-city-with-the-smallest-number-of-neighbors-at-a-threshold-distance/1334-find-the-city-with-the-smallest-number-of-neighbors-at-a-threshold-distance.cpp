bcda97ddb4886711f0887424901e7c852752b618
quarkshiv <quark.shiv@gmail.com>
2026-05-28 23:39:40 +0530

    Time: 32 ms (10.98%), Space: 20 MB (20.68%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
-  int dijikstra(int node, vector<vector<pair<int,int>>>& adj, int thres){
-    priority_queue<pair<int,int>, vector<pair<int,int>>,greater<>> pq;
-    pq.push({0,node});
-    set<int>vis;
-    while(!pq.empty()){
-        int dist = pq.top().first;
-        int cur = pq.top().second;
-        pq.pop();
-
-        if(vis.count(cur)) continue;
-        vis.insert(cur);
-
-        for(auto it : adj[cur]){
-            if(dist + it.second <= thres){
-                pq.push({dist + it.second, it.first});
-            }
-        }
-    }
-    return vis.size() - 1;
-}
-    int findTheCity(int n, vector<vector<int>>& edges, int thres) {
+  int diju(int node,vector<vector<pair<int,int>>>&adj,int dt,int n){
+    vector<int>dist(n,dt+1);
+     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+     pq.push({0,node});
+     dist[node]=0;
+     int cnt=0;
+     while(!pq.empty()){
+        int dst = pq.top().first;
+         int node=  pq.top().second;
+         pq.pop();
+         for(auto it : adj[node]){
+             if(dst+it.second<=dist[it.first]&& dst+it.second<=dt){
+                  dist[it.first]=dst + it.second;
+                  pq.push({dist[it.first],it.first});
+             }
+         }
+     }
+     for(int i=0;i<n;i++){
+         if(i!=node && dist[i]<=dt)cnt++;
+     }
+     return cnt;
+  }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        int ans=INT_MAX;
+        int node=-1;
         vector<vector<pair<int,int>>>adj(n);
-        for(auto it: edges){
-            adj[it[0]].push_back({it[1],it[2]});
-              adj[it[1]].push_back({it[0],it[2]});
+        for(auto it : edges){
+           adj[it[0]].push_back({it[1],it[2]});
+           adj[it[1]].push_back({it[0],it[2]});
         }
-        int mini=n;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            int nb = dijikstra(i,adj,thres);
-            if(nb<=mini){
-                mini=nb;
-                ans = i;
-            }
-        }
-        return ans;
+       for(int i=0;i<n;i++){
+          int val = diju(i,adj,distanceThreshold,n);
+          if(val<=ans){
+            ans= val;
+            node = i;
+          }
+       }
+       return node;
     }
 };
