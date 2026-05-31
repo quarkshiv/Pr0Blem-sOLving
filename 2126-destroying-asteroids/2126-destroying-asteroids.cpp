62d23d727d17d58a73cb2a6f474678cd8526dae7
quarkshiv <quark.shiv@gmail.com>
2026-05-31 13:46:28 +0530

    Time: 41 ms (13.87%), Space: 106.5 MB (99.12%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    bool asteroidsDestroyed(long long mass, vector<int>& asteroids) {
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         sort(asteroids.begin(),asteroids.end());
-        int cnt=0;
-            for(int i=0;i<asteroids.size();i++){
-            if(asteroids[i]<=mass){
-                mass+=asteroids[i];
-                cnt++;
-            }
-            else break;
+        long long val=mass;
+        for(auto it : asteroids){
+             if(it>val)return false;
+             else{
+                val+=it;
+             }
         }
-        return cnt==asteroids.size();
+        return true;
     }
 };
