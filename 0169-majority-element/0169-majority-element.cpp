4969353d068eead0f65337ec69ae06f7db101593
quarkshiv <quark.shiv@gmail.com>
2026-06-08 00:13:49 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.06%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int ele;
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+              if(cnt==0){
+                ele = nums[i];
+                cnt=1;
+              }
+              else if(nums[i]==ele)cnt++;
+              else cnt--;
+        }
+        return ele;
+    }
+};
