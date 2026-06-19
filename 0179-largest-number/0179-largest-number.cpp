300e89958099ed26321ff39f5a1992d68df5e28a
quarkshiv <quark.shiv@gmail.com>
2026-06-19 22:58:14 +0530

    Time: 8 ms (9.94%), Space: 16.3 MB (87.73%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),[](int a,int b){
+            return to_string(a)+to_string(b)>to_string(b)+to_string(a);
+        });
+        string ans;
+        for(auto it : nums){
+              ans+=to_string(it);
+        }
+        if(ans[0]=='0')return "0";
+        return ans;
+    }
+};
