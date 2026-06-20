df1ad59db914faf727ed0056f3fa902557c44b66
quarkshiv <quark.shiv@gmail.com>
2026-06-20 18:33:14 +0530

    Time: 15 ms (33.66%), Space: 14.2 MB (47.3%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char,int>>st;
+        for(auto it :s){
+               if(!st.empty() &&st.top().first==it && st.top().second<k){
+            st.top().second++;
+               if(st.top().second==k){
+                    st.pop();
+               }}
+               else st.push({it,1});
+        }
+        string ans;
+        while(!st.empty()){
+              while(st.top().second--)ans+=st.top().first;
+              st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
