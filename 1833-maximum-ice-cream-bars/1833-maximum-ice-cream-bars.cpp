a036f26a03ee09acf1150e57139932261e780a73
quarkshiv <quark.shiv@gmail.com>
2026-06-21 09:14:24 +0530

    Time: 35 ms (49.96%), Space: 80.4 MB (37.51%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
+    // int f(int ind,vector<int>& costs,int coins,vector<vector<int>>&dp){
+    //       if(ind==costs.size()){
+    //           return 0;
+    //       }
+    //       int nottake = f(ind+1,costs,coins,dp);
+    //       int take=0;
+    //         if(coins>=costs[ind]){
+    //               take=1+ f(ind+1,costs,coins-costs[ind],dp);
+    //         }
+    //         return dp[ind][coins]=max(take,nottake);
+    // }
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int i=0;
-        int n = costs.size();
-        while(i<n && coins>=0){
-              if(coins>=costs[i]){
-                   coins-=costs[i];
-                       i++;
-              }
-              else break;
-        }
-        return i;
+    //     int n =costs.size();
+    //     vector<vector<int>>dp(n+1,vector<int>(coins+1,-1));
+    //     return f(0,costs,coins,dp);
+     sort(costs.begin(),costs.end());
+     int ind=0;
+     while(ind<costs.size()){
+          if(costs[ind]<=coins){ coins-=costs[ind];
+           ind++;
+     }
+     else break;
+     }
+     if(ind==0)return 0;
+     return ind;
     }
 };
